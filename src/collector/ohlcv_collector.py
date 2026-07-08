11c0472423bf2b70debd4a27bad38915cdf740cc
manipenumajji <manipenumajji0@gmail.com>
2026-07-08 22:16:34 +0530

    completed the abstraction layer

--- src/collector/ohlcv_collector.py
+++ src/collector/ohlcv_collector.py
@@ -1,6 +1,15 @@
 import ccxt
+import pandas as pd
 class OHLCVcollector:
     def __init__(self):
         self.exchange=ccxt.binance()
-collector=OHLCVcollector()
-print(type(collector.exchange))        
+    def fetch_ohlcv(self):
+        data=self.exchange.fetch_ohlcv("BTC/USDT","1h",100)
+        return data
+    def create_dataframe(self,data):    
+        dataframe=pd.DataFrame(data,cloumns=["timestamp","open","high","low","close","volume"])
+        dataframe["timestamp"]=pd.to_datetime(dataframe["timestamp"],unit="ms")
+        return dataframe
+    def save_to_csv(self,dataframe):    
+        self.to_csv("data\raw\BTC_USDT_1h.csv",index=False)
+       
--- src/collector/test_ccxt.py
+++ src/collector/test_ccxt.py
@@ -1,10 +1,11 @@
-import ccxt
-import pandas as pd
-import os
-exchange=ccxt.binance()
-data=exchange.fetch_ohlcv("BTC/USDT","1h",limit=5)
-columns=["timestamp","open","high","low","close","volume"]
-dataframe=pd.DataFrame(data,columns=columns)
-dataframe["timestamp"]= pd.to_datetime(dataframe["timestamp"],unit="ms")
-dataframe.to_csv("data/raw/BTC_USDT_1h.csv",index=False)
-print(dataframe)
+from abc import ABC, abstractmethod
+class BaseExchange(ABC):
+    @abstractmethod
+    def fetch_ohlcv(self):
+        pass
+    @abstractmethod
+    def get_balance(self):
+        pass
+class BinanceExchange(BaseExchange):
+    pass    
+exchange = BinanceExchange()    
